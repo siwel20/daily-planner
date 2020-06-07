d9ca0f6f6404b072d3354963857165a89d65a522
siwel20 <plthrock@gmail.com>
2020-06-07 13:14:08 +0800

    adjusted language card position

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,3 +4,20 @@ const m = moment();
 // date at top
 var dateAtTop = document.querySelector("#currentDay");
 dateAtTop.textContent = m.format("dddd MMM DD, YYYY")
+var currentHour = moment().hour()
+var workHours = [9,10,11,12,13,14,15,16,17]
+var loadTime = function() {
+    for (let i = 0; i < 9; i++) {
+      if (workHours[i] < currentHour) {
+        document.getElementById(i).classList.add("bg-secondary");
+      }
+      else if (workHours[i] > currentHour) {
+        document.getElementById(i).classList.add("bg-success");
+      }
+      else if (workHours[i] === currentHour) {
+        document.getElementById(i).classList.add("bg-danger");
+      }
+        
+    }
+}
+loadTime();
