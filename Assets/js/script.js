9b51514a3060c02c57ee522b643cd92e6fdd4204
siwel20 <plthrock@gmail.com>
2020-06-08 19:57:17 -0600

    updated ReadMe and added if/else statement for local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,12 +22,23 @@ var loadTime = function() {
     }
 }
 
-var setTask = function() {
-  document.querySelector("textarea").addEventListener("click", function(event) {
-    const task = document.querySelector("#form-control").value;
-    console.log(event);
+  document.querySelector("button").addEventListener("click", function(e) {
+    const task = document.querySelector(".form-control").value;
+    
+    let formControl;
+    if(localStorage.getItem("formControl") === null) {
+      formControl = [];
+    } else {
+      formControl = JSON.parse(localStorage.getItem("formControl"));
+    }
+
+    formControl.push(task);
+
+    localStorage.setItem("formControl", JSON.stringify(formControl));
+    console.log(task);
+    console.log(formControl);
+
   })
-}
 
 
 loadTime();
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # daily-planner
+Daily Planner for the work day. 
+
+## Description
+The planner is to help you organize and track your project through out a normal 9 am to 5 pm job. 
+
+The tasks are color coded based on the time of the day. 
+Gray represents time that has past. 
+Red represent the current time.
+Green represent future time. 
+
+The date and time are based on your systems date and time. 
+
+## Future updates
+Will include the following:
+* Improvement to overall look.
+* Ways to schedule multiple days, weeks, months etc...
+* Fix localStorage bugs so it is working correctly.  
